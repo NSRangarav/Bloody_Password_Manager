94706e8f63d5c00750840cbee547345605ac6bb5
Rangaramanujam N S <rangaramanujamsri@gmail.com>
2024-09-18 14:36:48 +0000

    commit

---
+++ index.js
@@ -0,0 +1,10 @@
+//when the form is submitted
+document.getElementById("passwordForm").addEventListener("submit", function(event){
+    //this will be preventing event defaults
+    event.preventDefault();
+
+    //Get the values entered by the user
+    let website = document.getElementById("website").value;
+    
+
+}
